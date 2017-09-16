36e35f687f1dfbce1f7ace172bb13ef31a6f3af2
Miles Hingston <miles@jumpkick.io>
2017-09-16 09:20:05 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,44 @@
+# email-checker
+
+Check emails for typos, verify syntax and whether the domain has an MX record.
+
+# Install
+> npm install mhingston/email-checker
+	
+# Usage
+
+```javascript	
+const EmailChecker = require('email-checker');
+```
+
+## MX Record Check
+
+Returns: Promise
+* resolved {Boolean} Whether an MX entry exists
+* rejected {Object} Error object
+
+```javascript	
+EmailChecker.mxCheck({email: 'me@domain.com', op	tions: {});
+```
+
+You can also optionally pass in a [options object](https://github.com/yahoo/dnscache#configuration).
+
+## Email Spell Check
+
+Returns: Promise
+* resolved {String} Suggested mail address
+* rejected {Object} Error object
+
+```javascript	
+EmailChecker.spellCheck({email: 'me@domain.com', options: {});
+```
+
+You can also optionally pass in a [options object](https://github.com/mailcheck/mailcheck#usage-without-jquery).
+
+## Email Syntax Check
+
+Returns: Boolean
+
+```javascript	
+EmailChecker.syntaxCheck({email: 'me@domain.com'});
+```
---
+++ index.js
@@ -0,0 +1,45 @@
+const dnscache = require('dnscache');
+const Mailcheck  = require('mailcheck');
+const validator = require('validator');
+
+const EmailCheckerer =
+{
+    mxCheck: ({email, options}) =>
+    {
+        return new Promise((resolve, reject) =>
+        {
+            options = options || {enable: true, ttl: 300, cachesize: 1000};
+            const dns = dnscache(options);
+            const domain = email.split('@')[1];
+    
+            dns.resolveMx(domain, (error, addresses) =>
+            {
+                if(error)
+                {
+                    return reject(error);
+                }
+
+                return resolve(addresses.length > 0);
+            });
+        });
+    },
+
+    spellCheck: ({email, options}) =>
+    {
+        return new Promise((resolve, reject) =>
+        {
+            options = options || {};
+            options.email = email;
+            options.suggested = (suggestion) => resolve(suggestion.full);
+            options.empty = () => resolve(email);
+            return Mailcheck.run(options);
+        });
+    },
+
+    syntaxCheck: ({email}) =>
+    {
+        return validator.isEmail(email);
+    }
+}
+
+module.exports = EmailCheckerer;
